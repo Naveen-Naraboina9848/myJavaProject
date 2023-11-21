865eac419a7230e39b878d9e13904fa35122b533
Naveen-Naraboina9848@ <naveennaraboina24680@gmail.com>
2023-11-21 11:19:25 +0530

    welcome .java file is updated

--- src/com/edubridge/app/Welcome.java
+++ src/com/edubridge/app/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 
-		System.out.println("welcome to java get out of my mother land my foot");
+		System.out.println("welcome to java get out of my mother land my foot useless fellow");
 	}
 
 }
