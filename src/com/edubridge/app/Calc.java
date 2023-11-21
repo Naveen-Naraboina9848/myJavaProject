631ccde420268eec4b090eea4ad7bad16ddd2aab
Naveen-Naraboina9848@ <naveennaraboina24680@gmail.com>
2023-11-21 11:31:36 +0530

    calc.java is updated

--- src/com/edubridge/app/Calc.java
+++ src/com/edubridge/app/Calc.java
@@ -1,14 +1,8 @@
 package com.edubridge.app;
 
 public class Calc {
-<<<<<<< HEAD
+
 	public int findSquare(int num) {
 		return num*num;
 	}
-
-=======
-        public int findSquare(int num){
-        return num*num;
-        }             
->>>>>>> branch 'master' of https://github.com/Naveen-Naraboina9848/myJavaProject.git
 }
